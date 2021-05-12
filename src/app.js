d2206c920f886ef9da67387c5f69f04b50b041e9
TeddyKiryakova <teakiryakova@gmail.com>
2021-05-12 11:54:12 +0100

    Search engine added

--- src/app.js
+++ src/app.js
@@ -34,8 +34,20 @@ iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
 let apiKey = "70aecad43749ddb42a27c8361beb973e";
-let city = "Brentwood,UK";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+ 
+search("Brentwood,UK");
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
