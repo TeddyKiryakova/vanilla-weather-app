241b32388372eb00cd88c13a1833ba258b99311a
TeddyKiryakova <teakiryakova@gmail.com>
2021-05-19 12:04:51 +0100

    HTML injected in JS

--- src/app.js
+++ src/app.js
@@ -14,6 +14,32 @@ function formatDate(timestamp) {
 
 }
 
+function displayForecast() {
+let forecastElement = document.querySelector("#forecast");
+
+let forecastHTML= `<div class="row">`;
+let days = ["Thu", "Fri", "Sat", "Sun"];
+days.forEach(function (day) {
+forecastHTML = 
+ forecastHTML +
+ `
+          <div class="col-2">
+              <div class="forecast-date">
+           ${day}
+             </div> <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="30" />
+            </br><div class="forecast-temperature">
+                <span class="forecast-max">18°</span>|<span class="forecast-min">12°</span>
+            </div>
+           </div>
+      
+   
+`;
+});
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+
+}
+
 function displayTemperature(response){
 
     console.log(response);
@@ -84,6 +110,7 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Brentwood,UK");
+displayForecast();
 
 //
 
